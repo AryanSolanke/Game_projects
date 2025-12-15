bdefc02c964c9609378b54edf51b95c5e6bb5b3e
Aryan Solanke <aryansolanke1812@gmail.com>
2025-12-15 20:13:24 +0530

    chore: Update README refactor: clean up Gambling_game.py (readability & control flow)

--- Gambling_game.py
+++ Gambling_game.py
@@ -1,95 +1,111 @@
 import random
 import textwrap
 
-balance = 3000
+#global variables
+MAX_GAMBLE=500
+JACKPOT_MIN=0
+JACKPOT_MAX=5
 
-def updt_bal(balance, gambleamt, gambleres):
+def get_choice(msg):
+    while True:
+        try:
+            return int(input(msg))
+        except ValueError:
+            print("Invalid input, please enter an integer")
+
+def print_Jackno_rules():
+        print(textwrap.dedent("""
+            ++==++jackpot number game guide++==++\n
+            Rule: Max $500/gamble per round.
+            How to Play:
+            1. Enter a number in given range (0-5),
+            2. Enter amount to be gambled,
+            3. If the number you entered is the jackpot number your invested amount is doubled,
+            4. Continue playing or exit the game.\n"""))
+
+
+def validate_gambleno(gambleno):
+    return JACKPOT_MIN<=gambleno<=JACKPOT_MAX
+
+def validate_gambleamt(gambleamt, balance):
+    return  0<gambleamt<=balance and gambleamt<=MAX_GAMBLE
+
+
+def update_bal(balance, gambleamt, gambleres):
     balance -= gambleamt
-
-    if gambleres==1:
+    if gambleres:
         balance += gambleamt*2
     return balance
 
 #Gambling logic
-def sgamble(balance, gambleamt, gambleno):
-    jackno = random.randint(0,5)
+def play_round(balance, gambleamt, gambleno):
+    jackno = random.randint(JACKPOT_MIN, JACKPOT_MAX)
 
-    if(jackno==gambleno):
+    gambleres = (jackno==gambleno)
+    balance = update_bal(balance, gambleamt, gambleres)
+    return balance, gambleres
+
+def play_jackpot(balance):
+    while True:
+            gambleamt = get_choice("\nEnter amount of money you want to gamble: ")
+            if not validate_gambleamt(gambleamt, balance):
+                print("Insufficient balance or invalid gamble amount(Check rules)")
+                print(f"Current balance = {balance}")
+                continue
+            break
+        
+
+    while True:
+            gambleno = get_choice("Guess the jackpot number: ")
+            if not validate_gambleno(gambleno):
+                print(f"Invalid guess, choose {JACKPOT_MIN}-{JACKPOT_MAX}")
+                continue
+            break
+
+    balance, gambleres = play_round(balance, gambleamt, gambleno)
+
+    if gambleres:
         print("Congratulations you won the jackpot!!")
         print(f"Your gambled ${gambleamt} is doubled!!")
-        balance = updt_bal(balance, gambleamt, 1)
     else:
         print(f"Oops, No luck!\nYou lost ${gambleamt} :(")
-        print(f"Jackpot number was: {jackno}")
-        balance = updt_bal(balance, gambleamt, 0)
     return balance
 
+def main():
 #User inputs
-name = str(input("Enter your name: "))
-print(f"Welcome to gambling games, {name}.")
-print("Note: It's always recommended to check account balance before starting a gamble.")
-while True:
-    print("""\n|====->Menu<-====|\n1. Choose a gambling game.\n2. Check account balance\n3. Exit""", end="\n")
-    main_ch= int(input("Enter your choice: "))
+    balance = 3000
+    name = str(input("Enter your name: "))
+    print(f"Welcome to gambling games, {name}.")
+    while True:
+        print("""\n|====->Menu<-====|\n1. Jackpot number gambling.\n2. Exit""", end="\n")
+        game_ch= get_choice("Enter your choice: ")
+    #Game choice
+        match game_ch:
+            case 1:
+                print("\nInitial account balance = $3000")
+                print_Jackno_rules()
+                while True:
+                    print("|====->Menu<-====|\n1.Start\n2. Quit")
+                    base_choice = get_choice("Enter your choice: ")
+                    match base_choice:
+                        case 1:
+                            balance = play_jackpot(balance)
+                            print(f"New balance = {balance}")
+                        case 2:
+                            print("\nBye have a nice day! :D") 
+                            break
+                        case _:
+                            print("Please enter a valid integer. Choose 1-2")
+            case 2:
+                print("Hoping to see you again ^-^")
+                break
+            case _:
+                print("Invalid choice, Choose 1-2")
+                continue
 
-#main options choice
-    match main_ch:
-        case 1:
-        #Game selection
-            while True:
-                print("""\n|====->Games Menu<-====|\n1. Jackpot number gambling.\n2. Exit""", end="\n")
-                game_choice = int(input("Enter your choice: "))
-                match game_choice:
-                    case 1:
-                    #Base choice
-                        print("\nRule: Maximum amount of $500/gamble can be invested in this game.")
-                        while True:
-                            print("\n1. How to play\n2. Start\n3. Exit")
-                            action_choice = int(input("Enter your choice: "))
-                            match action_choice:
-                                case 1:
-                                    print(textwrap.dedent("""
-                                                        ++==++jackpot number game guide++==++
-                                                        1. Enter a number in given range (1-5),
-                                                        2. Enter amount to be gambled,
-                                                        3. If the number you entered is the jackpot number your invested amount is doubled,
-                                                        4. Continue playing or exit the game.\n"""))
-                                case 2:
-                                    while(1):
-                                       gambleamt = int(input("\nEnter amount of money you want to gamble: "))
-                                       if gambleamt > balance:
-                                            print("Insufficient balance.")
-                                            continue
-                                       if(gambleamt>500):
-                                            print("Invalid!, remember the rule: you cant gamble more than $500/gamble")
-                                            continue
-                                       else:
-                                            break
-                                           
-                                    print("\nJackpot number is between 0-5 (0&5 included)")
-                                    while(1):
-                                        gambleno = int(input("Guess the jackpot number: "))
-                                        if gambleno<0 or gambleno>5:
-                                            print("Invalid guess")
-                                            continue 
-                                        else:
-                                            break
-                                    balance = sgamble(balance, gambleamt, gambleno)
-                                case 3:
-                                    print("Bye have a nice day! :D")
-                                    break
-                                case _:
-                                    print("Invalid choice, Please select between 1-3")
-                    case 2:
-                        print("More games coming soon stay tuned!")
-                        break
-                    case _:
-                        print("Invalid choice, Please select between 1-2")
+if __name__ == "__main__":
+    try:
+        main()
+    except (KeyboardInterrupt, EOFError):
+        print("\nGoodbye!")
 
-        case 2:
-            print(f"\nBalance = ${balance}")
-        case 3:
-            print("Hoping to see you again ^-^")
-            break
-        case _:
-            print("Invalid choice, Please select between 1-3")
